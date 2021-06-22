5592168363a51e0d9d2e260920ff2e6e7a562bdf
Nicolas Salazar <Nico1608@Nicolass-Air.cable.rcn.com>
2021-06-22 11:13:17 -0400

    user/auth

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+
+@user = User.create!(username: 'Nico', email: 'nicolasd1323@gmail.com', password: '123456')
+
+puts "#{User.count} users created"
