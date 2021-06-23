33dba0200ebc584d10e0eaa2fec3c9f783c91c27
Nicolas Salazar <Nico1608@Nicolass-Air.cable.rcn.com>
2021-06-23 09:33:41 -0400

    client structure

---
+++ client/src/screens/PetEdit.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function PetEdit(props) {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default PetEdit;
---
+++ client/src/screens/PetsCreate.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function PetsCreate(props) {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default PetsCreate;
